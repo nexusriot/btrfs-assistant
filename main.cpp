c981e00c280690d49c0d87c92334febfcbad18b3
dalto <dalto@fastmail.com>
2022-04-08 11:51:49 -0500

    Start development of 1.4

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 int main(int argc, char *argv[]) {
     QApplication ba(argc, argv);
     QCoreApplication::setApplicationName(QCoreApplication::translate("main", "Btrfs Assistant"));
-    QCoreApplication::setApplicationVersion("1.3");
+    QCoreApplication::setApplicationVersion("1.4");
     ba.setWindowIcon(QIcon(":/btrfs-assistant.png"));
 
     QCommandLineParser parser;
